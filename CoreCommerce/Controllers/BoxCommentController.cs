3b5e911871ba93975f88477faea67482340e8e7c
Cray Jaeger <cray@alternascript.com>
2017-10-23 13:53:03 -0500

    Adds boxcommentcontroller

---
+++ CoreCommerce/Controllers/BoxCommentController.cs
@@ -0,0 +1,68 @@
+﻿using CoreCommerce.Models;
+using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CoreCommerce.Controllers
+{
+    [BasicAuthentication]
+    public class BoxCommentController : ApiController
+    {
+        private ApplicationContext context = new ApplicationContext();
+        private IBoxCommentRepository boxComments;
+
+        public BoxCommentController()
+        {
+            boxComments = new BoxCommentRepository(context);
+        }
+
+        // GET: api/Box/5
+        [SwaggerOperation("GetByID")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public IEnumerable<BoxComment> Get(int id)
+        {
+            return boxComments.GetBoxComments(id);
+        }
+
+        public BoxComment GetComment(int comment_id)
+        {
+            return boxComments.GetBoxCommentById(comment_id);
+        }
+
+        // POST: api/Box
+        [SwaggerOperation("Create")]
+        [SwaggerResponse(HttpStatusCode.Created)]
+        public BoxComment Post([FromBody]BoxComment comment)
+        {
+            comment.created = DateTime.Now;
+            comment.updated = DateTime.Now;
+
+            return boxComments.CreateBoxComment(comment);
+        }
+
+        // PUT: api/Box
+        [SwaggerOperation("Update")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public void Put([FromBody]BoxComment comment)
+        {
+            comment.updated = DateTime.Now;
+
+            boxComments.UpdateBoxComment(comment);
+        }
+
+        // DELETE: api/Box/5
+        [SwaggerOperation("Delete")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public void Delete(int id)
+        {
+            boxComments.DeleteBoxComment(id);
+        }
+    }
+}
