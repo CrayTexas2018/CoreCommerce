0e2744c965b5c2f68961a8a4681e8225f0b0be94
Cray Jaeger <cray@alternascript.com>
2017-10-30 19:17:49 -0500

    adds checkout controller

--- CoreCommerce/Controllers/CheckoutController.cs
+++ CoreCommerce/Controllers/CheckoutController.cs
@@ -4,36 +4,73 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using CoreCommerce.Models;
 
 namespace CoreCommerce.Controllers
 {
     public class CheckoutController : ApiController
     {
-        // GET: api/Checkout
-        public IEnumerable<string> Get()
+        /*
+         IEnumerable<Checkout> GetCheckouts();
+        IEnumerable<Checkout> GetUserCheckouts(int user_id);
+        Checkout GetCheckout(int checkout_id);
+        Checkout CreateCheckout(PostCheckout postCheckout);
+        Checkout GetLastUserCheckout(int user_id);
+        void DeleteCheckout(int checkout_id);
+        void CompleteCheckout(int checkout_id);
+        void UpdateCheckout(Checkout checkout);
+        */
+
+        private ApplicationContext context = new ApplicationContext();
+        private ICheckoutRepository checkouts;
+
+        public CheckoutController()
         {
-            return new string[] { "value1", "value2" };
+            checkouts = new CheckoutRepository(context);
         }
 
-        // GET: api/Checkout/5
-        public string Get(int id)
+        // GET: /api/checkout
+        public IEnumerable<Checkout> Get()
         {
-            return "value";
+            return checkouts.GetCheckouts();
         }
 
-        // POST: api/Checkout
-        public void Post([FromBody]string value)
+        [Route("api/checkout/user/{user_id}")]
+        public IEnumerable<Checkout> GetUserCheckouts(int user_id)
         {
+            return checkouts.GetUserCheckouts(user_id);
         }
 
-        // PUT: api/Checkout/5
-        public void Put(int id, [FromBody]string value)
+        public Checkout GetCheckout(int checkout_id)
         {
+            return checkouts.GetCheckout(checkout_id);
         }
 
-        // DELETE: api/Checkout/5
-        public void Delete(int id)
+        public Checkout Post(PostCheckout postCheckout)
         {
+            return checkouts.CreateCheckout(postCheckout);
+        }
+
+        [Route("api/checkout/user/{user_id}/last")]
+        public Checkout GetLastUserCheckout(int user_id)
+        {
+            return checkouts.GetLastUserCheckout(user_id);
+        }
+
+        public void Delete(int checkout_id)
+        {
+            checkouts.DeleteCheckout(checkout_id);
+        }
+
+        [Route("api/checkout/complete/{checkout_id}")]
+        public void CompleteCheckout(int checkout_id)
+        {
+            checkouts.CompleteCheckout(checkout_id);
+        }
+
+        public void Put(Checkout checkout)
+        {
+            checkouts.UpdateCheckout(checkout);
         }
     }
 }
