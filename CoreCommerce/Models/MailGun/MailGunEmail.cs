109ecc38fdd60387ea53acbfb18db397e9abf87d
Cray Jaeger <cray@alternascript.com>
2017-11-06 14:53:00 -0600

    mailgun stuff

---
+++ CoreCommerce/Models/MailGun/MailGunEmail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreCommerce.Models.MailGun
+{
+    public class MailGunEmail
+    {
+        public List<String> to_emails { get; set; }
+
+        public List<String> cc_emails { get; set; }
+
+        public List<String> bcc_email { get; set; }
+
+        public string from_name { get; set; }
+
+        public string subject { get; set; }
+
+        public string html_body { get; set; }
+    }
+}
